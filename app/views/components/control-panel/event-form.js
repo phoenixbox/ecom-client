77a08a171e9b0e2b2a6d7196e792011793b38d70
Shane Rogers <rogerssh@tcd.ie>
2015-09-14 00:35:49 -0700

    control panel event form

---
+++ app/views/components/control-panel/event-form.js
@@ -0,0 +1,11 @@
+import React from 'react/addons';
+
+let EventForm = React.createClass({
+  render() {
+    return (
+      <div className="event-form col-xs-12">{`Event Form`}</div>
+    )
+  }
+})
+
+export default EventForm;
