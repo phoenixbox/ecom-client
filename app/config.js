63985701156475224049dcf20cbc359c2b2313f6
Shane Rogers <rogerssh@tcd.ie>
2015-09-18 01:23:24 -0700

    Update config vars and setup intercom instance

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 /*eslint-env node */
-export const RAILSRoot = process.env.NODE_ENV === 'production' ? 'https://coffeeslots-server.herokuapp.com': 'http://127.0.0.1:5000';
-export const HAPIRoot = process.env.NODE_ENV === 'production' ? 'https://coffeeslots.herokuapp.com': 'http://127.0.0.1:3700';
-export const MAPBOX_KEY = 'pk.eyJ1IjoicGhvZW5pYm94IiwiYSI6ImNpZWo3bTcydzAwdHRza2tnY3h6azBtbjEifQ.G-HmM7UmDxu8fLPTVRYCQA';
+export const RAILSRoot = process.env.NODE_ENV === 'production' ? 'https://eventercom-rails.herokuapp.com': 'http://127.0.0.1:5000';
+export const HAPIRoot = process.env.NODE_ENV === 'production' ? 'https://eventercom.herokuapp.com': 'http://127.0.0.1:3700';
+export const MAPBOX_KEY = process.env.MAPBOX_KEY || 'pk.eyJ1IjoicGhvZW5pYm94IiwiYSI6ImNpZWo3bTcydzAwdHRza2tnY3h6azBtbjEifQ.G-HmM7UmDxu8fLPTVRYCQA';
